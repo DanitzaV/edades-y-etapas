55cc88184c67f7b30dcf79efd5339b2c3526923f
viviana <dmpviviana@gmail.com>
2018-05-26 12:31:29 -0400

    terminacion de problema

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var pregunta =  prompt('hola?');
 console.log(pregunta)
-if(pregunta % 1 == 0 && pregunta !== " " && pregunta != 0){
+if(pregunta % 1 == 0 && pregunta !== " " && pregunta != 0 ){
     if (pregunta > 0 && pregunta <= 3) {
         alert("you're a toddler");
+    }else if (pregunta > 3 && pregunta <=5) {
+        alert("you're a Preschool");
+    }else if(pregunta > 5 && pregunta <= 12){
+        alert("you're a Gradeschooler")
+    }else if(pregunta >12 && pregunta <=18){
+        alert("you're a Teen");
+    }else if(pregunta > 18 && pregunta <=21){
+        alert("you´re a Young Adult")
     }
 }else{
     alert('ERROR INGRESA UN NUMERO ');
